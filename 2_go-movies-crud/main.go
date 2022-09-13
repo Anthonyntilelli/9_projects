36c725c395708cefdcd35258108e6b7ae8a16848
Anthony Tilelli <Anthony@tilelli.me>
2022-09-12 21:38:20 -0400

    corrected lesson 2

--- 2_go-movies-crud/main.go
+++ 2_go-movies-crud/main.go
@@ -28,6 +28,7 @@ var movies []Movie
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
+	fmt.Println("GET all movies")
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
@@ -36,6 +37,7 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	for _, item := range movies {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
+			fmt.Println("Got a movie ", item)
 			return
 		}
 	}
@@ -48,6 +50,7 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	movie.ID = strconv.Itoa(rand.Intn(1000000000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
+	fmt.Println("Created a movie ", movie)
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
@@ -94,12 +97,12 @@ func main() {
 		Director: &Director{Firstname: "Steve", Lastname: "Smith"},
 	})
 
-	r.handleFunc("/movies", getMovies).Methods("GET")
-	r.handleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.handleFunc("/movies", createMovie).Methods("POST")
-	r.handleFunc("/movies/{id}", updateMovie).Methods("PUT")
-	r.handleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
-	fmt.Printf("Starting server at port 8080")
+	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
